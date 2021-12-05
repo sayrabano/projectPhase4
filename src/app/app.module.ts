28c0048decc2c8f0d64ce12da0bfb42104c54775
sayra <sayra.bano8888@gmail.com>
2021-12-05 16:49:22 +0530

    Working on dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './sharepage/navbar/navbar.component';
+
 import { FooterComponent } from './sharepage/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchfoodComponent } from './pages/searchfood/searchfood.component';
@@ -17,16 +17,17 @@ import { ManageFoodItemsComponent } from './pages/manage-food-items/manage-food-
 
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
-   NavbarComponent,
+
     FooterComponent,
     HomeComponent,
     SearchfoodComponent,
     AdminloginComponent,
     AdminDashboardComponent,
-    ManageFoodItemsComponent
+    ManageFoodItemsComponent,
 
     
     
